1750e5652f4be5957ddfe50890e35bd12a0e27d6
GiuliaMontani <giulia.montani@mail.polimi.it>
2022-05-23 20:48:00 +0100

    Update cluser_all.R

--- Giulia/cluser_all.R
+++ Giulia/cluser_all.R
@@ -1,6 +1,6 @@
 setwd("~/GitHub/AS_Project_2022")
 
-#import data
+#import data (line 3-116, only RUN)
 data_patient= read.delim(file.path("Dataset/data_clinical_patient.txt"), header = TRUE, comment.char = '#')
 data_treatment_auc= read.delim(file.path("Dataset", 'data_drug_treatment_auc.txt'), header = TRUE, comment.char = '#')
 data_sample= read.delim(file.path("Dataset",'data_clinical_sample.txt'), header = TRUE, comment.char = '#')
@@ -122,32 +122,127 @@ cancer_data_treatment_auc = t(cancer_data_treatment_auc)
 M = as.data.frame(cancer_data_treatment_auc)
 M <- as.matrix(cancer_data_treatment_auc)
 
-de<- dist(M,method='euclidean')
-dm<- dist(M,method='manhattan')
-dc <- dist(M,method='canberra')
-
-M_.es <- hclust(de, method='single')
-M_.ea <- hclust(de, method='average')
-M_.ec <- hclust(de, method='complete')
-
-M_.ms <- hclust(dm, method='single')
-M_.ma <- hclust(dm, method='average')
-M_.mc <- hclust(dm, method='complete')
-
-M_.cs <- hclust(dc, method='single')
-M_.ca <- hclust(dc, method='average')
-M_.cc <- hclust(dc, method='complete')
-
-
+#Plot of the variance of the treatment, i take as  treshold 0.06, feel free to change
 M_var= apply(M,2,var)
 x11()
 plot(M_var)
 abline(h=0.06)
+M_0.06= M[,M_var>0.06]
+
+#All cluster 
+de_0.06<- dist(M_0.06,method='euclidean')
+dm_0.06<- dist(M_0.06,method='manhattan')
+dc_0.06 <- dist(M_0.06,method='canberra')
+
+M_.es_0.06 <- hclust(de, method='single')
+M_.ea_0.06 <- hclust(de, method='average')
+M_.ec_0.06 <- hclust(de, method='complete')
+
+M_.ms_0.06 <- hclust(dm, method='single')
+M_.ma_0.06 <- hclust(dm, method='average')
+M_.mc_0.06 <- hclust(dm, method='complete')
+
+M_.cs_0.06 <- hclust(dc, method='single')
+M_.ca_0.06 <- hclust(dc, method='average')
+M_.cc_0.06 <- hclust(dc, method='complete')
+
+#dendogram
+x11()
+par(mfrow=c(1,3))
+plot(M_.es_0.06,labels= FALSE,main="ES")
+rect.hclust(M_.es_0.06, k=7)
+plot(M_.ea_0.06,labels= FALSE,main="EA")
+rect.hclust(M_.ea_0.06, k=7)
+plot(M_.ec_0.06,labels= FALSE,main="EC")
+rect.hclust(M_.ec_0.06, k=7)
+
+
+x11()
+par(mfrow=c(1,3))
+plot(M_.ms_0.06,labels= FALSE,main="MS")
+rect.hclust(M_.ms_0.06, k=7)
+plot(M_.ma_0.06,labels= FALSE,main="MA")
+rect.hclust(M_.ma_0.06, k=7)
+plot(M_.mc_0.06,labels= FALSE,main="MC")
+rect.hclust(M_.mc_0.06, k=7)
+
+x11()
+par(mfrow=c(1,3))
+plot(M_.cs_0.06,labels= FALSE,main="CS")
+rect.hclust(M_.cs_0.06, k=7)
+plot(M_.ca_0.06,labels= FALSE,main="CA")
+rect.hclust(M_.ca_0.06, k=7)
+plot(M_.cc_0.06,labels= FALSE,main="CC")
+rect.hclust(M_.cc_0.06, k=7)
+
+#single doesn't make sense to me
+
+#compare the average
+x11()
+par(mfrow=c(1,3))
+plot(M_.ea_0.06,labels= FALSE,main="EA")
+rect.hclust(M_.ea_0.06, k=7)
+plot(M_.ma_0.06,labels= FALSE)
+rect.hclust(M_.ma_0.06, k=7)
+plot(M_.ca_0.06,labels= FALSE)
+rect.hclust(M_.ca_0.06, k=7)
+
+#compare the complete
+x11()
+par(mfrow=c(1,3))
+plot(M_.ec_0.06,labels= FALSE,main="EC")
+rect.hclust(M_.ec_0.06, k=7)
+plot(M_.mc_0.06,labels= FALSE,main="MC")
+rect.hclust(M_.mc_0.06, k=7)
+plot(M_.cc_0.06,labels= FALSE,main="CC")
+rect.hclust(M_.cc_0.06, k=7)
+
+
+k=7
+
+cluster.ec <- cutree(M_.ec_0.06, k=k) # euclidean-complete
+cluster.es <- cutree(M_.es_0.06, k=k) # euclidean-single
+cluster.ea <- cutree(M_.ea_0.06, k=k) # euclidean-average
+
+cluster.ms <- cutree(M_.ms_0.06, k=k) # manhattan-single
+cluster.ma <- cutree(M_.ma_0.06, k=k) # manhattan-average
+cluster.mc <- cutree(M_.mc_0.06, k=k) # manhattan-complete
+
+cluster.cs <- cutree(M_.cs_0.06, k=k) # canberra-single
+cluster.ca <- cutree(M_.ca_0.06, k=k) # canberra-average
+cluster.cc <- cutree(M_.cc_0.06, k=k) # canberra-complete
+
+#I take the canberra-complete
+x11()
+plot(cluster.cc)
 
 library(heatmaply)
-M_0.06= M[,M_var>0.06]
 heatmaply(data.matrix(M_0.06),hclustfun= hclust,dist_method='canberra' ,hclust_method = 'complete',k_row=7)
 
+groups <- cluster.cc
+matrix_group= data.frame(cluster.cc)
 
+#index in the AUC matrix
+index_1= which(matrix_group==1)
+index_2= which(matrix_group==2)
+index_3= which(matrix_group==3)
+index_4= which(matrix_group==4)
+index_5= which(matrix_group==5)
+index_6= which(matrix_group==6)
+index_7= which(matrix_group==7)
 
+name=rownames(matrix_group)
+name1=name[index_1]
+name2=name[index_2]
+name3=name[index_3]
+name4=name[index_4]
+name5=name[index_5]
+name6=name[index_6]
+name7=name[index_7]
 
+#Understand if in the group we have the same part of the body or if they are different
+#In group 7 we have only HAEMATOPOIETIC_AND_LYMPHOID_TISSUE
+#In group 6 we have HAEMATOPOIETIC_AND_LYMPHOID_TISSUE, one OVARY, two LUNG, two BONE
+
+M7=M[index_7,]
+heatmap(M7)
